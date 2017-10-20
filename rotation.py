d81782675c0a06114a0f54f25a60e71b8b5ca276
Kunal Dargan <chakdekunal@gmail.com>
2017-10-20 07:50:08 +0530

    Update rotation.py

--- rotation.py
+++ rotation.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 19 17:07:54 2017
+@author: kd
+Create 2 empty strings as string1 and string2 : string1 is the original and string2 for checking if rotation is there
+2. loop through both the strings to input the string
+3. take length of string1 as l1 and string2 as l2
+4. concatenate string1 to itself as string1= string1 + string1
+5. If l1 is equal l2 
+6. Check if string2 is substring of concatenated string1 ,Boolean = True
+7. Else Boolean = false
+8. If Boolean is true : string2 is rotation of string1
 
-@author: DELL1
+ To check substring in python : if string2 in string1
+Or string1.find(string2) : which is true if string2 is in {concatenated} string1
+
 """
 str=""
 str=input("please enter the string :")
