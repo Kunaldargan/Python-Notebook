9d950c668c3d41f04572991eb3d20a84e1be5ec5
Kunal Dargan <chakdekunal@gmail.com>
2017-10-20 13:22:23 +0530

    Array_.py

---
+++ Array_.py
@@ -0,0 +1,26 @@
+""" how to take integer input in python
+2 methods of type casting"""
+
+num_array = []
+num=0
+num = input("Enter how many elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(num)):
+    n = input("num :")
+    num_array.append(n)
+
+print ('ARRAY: ',num_array)
+
+"""
+------------------------------------------------
+ num_array = []
+num = input("Enter how many elements you want:")
+print ('Enter numbers in array: ')
+for i in range(int(num)):
+    n = input("num :")
+    num_array.append(n)
+
+print ('ARRAY: ',num_array)
+
+"""
+
