9e7e1f5f65c26570f3285df61f9d1810994f3dad
Kunal Dargan <chakdekunal@gmail.com>
2017-10-20 07:49:12 +0530

    Update pallindrome.py

--- pallindrome.py
+++ pallindrome.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 19 16:31:23 2017
-
 @author: KD
 Pallindrome
+
+1. Create an empty string to hold the string
+2. loop through each character in the input string
+3. Inside the loop from zero to len(string) ie,length of string
+    -if string character (string[i]) moving from left to right is equal to string characters moving from right to left then return true
+    -else return false
+4. At the point of function call if returned value is true:string is in pallindrome
+
 """
 str=""
 str=input('Please enter string :')
